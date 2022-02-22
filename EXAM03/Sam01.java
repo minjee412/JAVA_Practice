85c850c299809ba648f22f3c16f0db91c2cbf7ff
minjee412 <88178872+minjee412@users.noreply.github.com>
2022-02-22 13:35:44 +0900

    firstcommit

---
+++ EXAM03/Sam01.java
@@ -0,0 +1,20 @@
+// import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class Sam01 {
+   public static void main(String[] args){
+       int i1 = 10; // 10진수
+       int i2 = 0b10; // 2진수
+       int i3 = 0x10; // 16진수
+       int i4 = 010; // 8진수
+
+
+       System.out.println(i1);
+       System.out.println(i2);
+       System.out.println(i3);
+       System.out.println(i4);
+       
+       //2진수
+       //8진수
+       //16진수
+   } 
+}
