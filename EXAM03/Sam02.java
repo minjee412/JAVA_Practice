71ab3326cc0b1cb720d4eeaf3bccef039d896e7c
minjee412 <88178872+minjee412@users.noreply.github.com>
2022-02-23 11:03:18 +0900

    2021.11.03

---
+++ EXAM03/Sam02.java
@@ -0,0 +1,14 @@
+// import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class Sam02 {
+    public static void main(String[] args){
+        int i1 = 0b000_0000_0000_0000_0000_0000_0000_0000_1100;
+        int i2 = -12;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i1 + i2);
+
+    } 
+ }
+ 
