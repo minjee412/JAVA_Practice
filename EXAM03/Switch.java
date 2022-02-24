ce66321083ff8ebfda2d90032d6178436052f88b
minjee412 <88178872+minjee412@users.noreply.github.com>
2022-02-24 13:16:44 +0900

    2022.02.24

---
+++ EXAM03/Switch.java
@@ -0,0 +1,11 @@
+public class Switch {
+    public static void main(String[] args) {
+        //1,   2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12
+        //31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+
+        int month = 1;
+        int day = 0;
+
+
+    }
+}
