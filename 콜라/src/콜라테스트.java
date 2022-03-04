368497e7b035614ac3e488e7d8b2f4daade51516
minjee412 <88178872+minjee412@users.noreply.github.com>
2022-03-04 18:42:27 +0900

    2022.03.04

---
+++ 콜라/src/콜라테스트.java
@@ -0,0 +1,13 @@
+public class 콜라테스트 {
+    
+    public static void main(String[] args) {
+        콜라 마실콜라1 = new 콜라();
+        // 콜라 마실콜라2 = new 콜라();
+        /*  클래스 초기화 블럭
+            인스턴스 초기화 블럭
+            생성자 호출
+
+            인스턴스 초기화 블럭
+            생성자 호출  */
+    }
+}
